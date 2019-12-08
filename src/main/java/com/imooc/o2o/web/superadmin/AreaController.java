62660bab2c2f84c688595d6b972764741f1725b1
taokang <1435703893@qq.com>
2019-12-08 22:18:25 +0800

    update

---
+++ src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
@@ -0,0 +1,32 @@
+package com.imooc.o2o.web.superadmin;
+
+import com.imooc.o2o.entity.Area;
+import com.imooc.o2o.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/superadmin")
+public class AreaController {
+
+    @Autowired
+    private AreaService areaService;
+
+    @RequestMapping(value = "/listarea")
+    @ResponseBody
+    private Map<String, Object> listArea() {
+        Map<String,Object> modelMap=new HashMap<String,Object>();
+        List<Area> list = areaService.getAreaList();
+        list=areaService.getAreaList();
+        modelMap.put("rows",list);
+        modelMap.put("total",list.size());
+        return modelMap;
+    }
+}
