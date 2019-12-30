81efc63e8081cab7db7ca501ef5452182d8e1106
taokang <1435703893@qq.com>
2019-12-31 00:12:13 +0800

    update

--- src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
@@ -1,5 +1,6 @@
 package com.imooc.o2o.dao;
 
+import com.imooc.o2o.BaseTest;
 import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.entity.PersonInfo;
 import com.imooc.o2o.entity.Shop;
@@ -10,33 +11,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
-public class ShopDaoTest {
+public class ShopDaoTest extends BaseTest {
     @Autowired
     private  ShopDao shopDao;
 
     @Test
     public void testInsertShop(){
-        System.out.println("insertShop test!");
-        Shop shop=new Shop();
-        ShopCategory shopCategory=new ShopCategory();
-        Area area=new Area();
-        PersonInfo personInfo=new PersonInfo();
-
-        personInfo.setUserId((long) 1);
-        area.setAreaId(1);
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId((long) 1);
+        area.setAreaId(2);
         shopCategory.setShopCategoryId(1L);
-
-        shop.setOwner(personInfo);
+        shop.setOwner(owner);
         shop.setArea(area);
         shop.setShopCategory(shopCategory);
-        shop.setShopName("测试店铺");
-        shop.setShopDesc("test1");
-        shop.setShopAddr("test2");
-        shop.setPhone("test3");
-        shop.setShopImg("test4");
+        shop.setShopName("测试的店铺");
+        shop.setShopDesc("test");
+        shop.setShopAddr("test");
+        shop.setPhone("test");
+        shop.setShopImg("test");
         shop.setCreateTime(new Date());
-        shop.setEnableStatus(1);
+        shop.setEnableStatus(0);
         shop.setAdvice("审核中");
-        System.out.println(shopDao.insertShop(shop));
+        int effectedNum = shopDao.insertShop(shop);
+        System.out.println(effectedNum);
     }
 }
